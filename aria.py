1d1cdcb564cca77c01c27cc736f70330ab348a48
i6o6i <gzhw734@gmail.com>
2019-08-02 17:49:10 +0800

    add profile feature

--- aria.py
+++ aria.py
@@ -4,22 +4,18 @@ import asyncio
 import websockets
 import json
 import argparse
-from pprint import pprint
-
 
 class ariafrontpy:
     def __init__(self,path):
         self.afp={}
         with open(path,'r') as afpconfig:
             self.afp=json.load(afpconfig)
-        self.options=self.afp['defaultoptions'][0]
+        self.options=self.afp['options'][0]
 
     def addtask(self,method,uri,options):
-        for (key,value) in options:
+        for key,value in options.items():
             self.options[key]=value
-        pprint(self.options)
         self.parse_json(method,uri,self.options)
-        pprint(self.jsondata)
         if self.afp['rpcaddr'].split('://')[0] == 'ws':
             asyncio.get_event_loop().run_until_complete(self.sendviaws(self.jsondata))
         elif self.afp['rpcaddr'].split('://')[0] == 'http': 
@@ -57,22 +53,45 @@ class ariafrontpy:
 
 def main():
     parser=argparse.ArgumentParser(description="python aria front")
-    parser.add_argument('-t','--type',help="download via torrent file path or uris",choices=['torrent','uris','t','u'],default='u',type=str)
-    parser.add_argument('-c','--conf',help="config file for ariafpy",nargs=1,default='./ariafpy.json',type=str)
-    parser.add_argument('-u','--uris',metavar='torrent or uris',help="uris of file you want to download",nargs='+',type=str)
-    parser.add_argument('-a','--options',help="aria options",default={},nargs=argparse.REMAINDER)
+    parser.add_argument('-m','--method',
+            help="download via torrent file path or uris",default='u',
+            choices=['torrent','uris','t','u'],type=str)
+    parser.add_argument('-s','--save',
+            help="save current profile of a task",
+            nargs='?',const='./profile.json',
+            metavar='profile path',type=str)
+    parser.add_argument('-c','--conf',help="config file for ariafpy",
+            metavar='config file path',nargs=1,type=str,
+            default='./ariafpy.json')
+    parser.add_argument('-p','--profile',nargs='?',type=str,
+            help="task profile used to store info of a task",
+            const='./profile.json')
+    parser.add_argument('-u','--uris',metavar='torrent or uris',
+            nargs='+',type=str,
+            help="uris of file you want to download",)
+    parser.add_argument('-a','--options',help="aria options",
+            default={},nargs=argparse.REMAINDER)
     args=parser.parse_args()
-    print(args)
-    options={}
-    it=iter(args.options)
-    for i in it:
-        options[i]=next(i)
 
     ariafpy=ariafrontpy(args.conf)
-    if args.type[0] == 'u':
-        ariafpy.addtask('uris',args.uris,options)
-    elif args.type[0] == 't':
-        ariafpy.addtask('torrent',args.uris,options)
+    if args.profile == None:
+        method= 'uris'if args.method[0] == 'u' else 'torrent'
+        uris=args.uris
+        options={}
+        it=iter(args.options)
+        for i in it:
+            options[i]=next(it)
+        ariafpy.addtask(method,uris,options)
+        if args.save != None:
+            ajson={'method':method,'uris':uris,
+                    'options':[options]}
+            with open(args.save,'w') as afile:
+                json.dump(ajson,afile,indent=4)
+    else:
+        with open(args.profile, 'r') as afile:
+            profile=json.load(afile)
+        ariafpy.addtask(profile['method'],profile['uris'],
+                profile['options'][0])
 
 if __name__ == "__main__":
     main()
